d8423d67c68e608dea4792c6021259b0ab7ceda9
AlexWebArchitect <alex.web.architect@gmail.com>
2017-06-11 14:30:16 +0300

    copy and paste game

---
+++ src/Square.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface Props {}
+interface State {}
+
+export default class Square extends React.Component <Props, State> {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
