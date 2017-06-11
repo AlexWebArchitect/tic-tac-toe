a9db5d3540e593e08516ab672ef4fb47903c1cbd
AlexWebArchitect <alex.web.architect@gmail.com>
2017-06-11 14:11:43 +0300

    create-typescript-app

--- src/Square.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Square(props) {
-  return (
-    <button className={props.sclass} onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
